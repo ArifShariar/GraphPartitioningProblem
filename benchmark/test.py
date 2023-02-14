089300f67afaeea8492b7fe5fa67c5e35cf060cd
Arif Shariar Rahman <1705095@ugrad.cse.buet.ac.bd>
2023-02-14 10:00:19 +0600

    updated gitignore and reformatted codes

--- benchmark/test.py
+++ benchmark/test.py
@@ -1,9 +1,8 @@
 import sys
+
 sys.path.append('..')
 
-from kernighan_lin.kernighan_lin import * 
-
-
+from kernighan_lin.kernighan_lin import *
 
 graph = load_data("../data/data.txt")
 print("Loaded graph with " + str(len(graph.vertices)) + " vertices and " + str(len(graph.edges)) + " edges.")
